c38a15ddf6d364f4c6e776581274cad345df8edc
rkskqdl-a11y <rkskqdl@gmail.com>
2025-08-20 22:33:10 +0900

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -1,8 +1,4 @@
-// functions/api.js
 export async function onRequest(context) {
-  // 이 친구는 /api 라는 주소로 찾아오면 응답할 거야.
-  // 나중에 쿠팡파트너스 아저씨랑 통화하는 방법을 알려줄게!
-
   return new Response("Hello from Functions!", {
     headers: { "Content-Type": "text/plain" },
   });
